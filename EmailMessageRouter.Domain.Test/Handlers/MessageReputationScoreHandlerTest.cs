f8f310a8dc7fb090219c4862faf08d9e96195d8e
Mario Dennis <mdennis876@gmail.com>
2020-02-05 22:13:56 -0500

    add a few test

---
+++ EmailMessageRouter.Domain.Test/Handlers/MessageReputationScoreHandlerTest.cs
@@ -0,0 +1,26 @@
+﻿using EmailMessageRouter.Domain.Handlers;
+using EmailMessageRouter.Domain.Model;
+using Moq;
+using NUnit.Framework;
+
+namespace EmailMessageRouter.Domain.Test.Handlers
+{
+    public class MessageReputationScoreHandlerTest
+    {
+        [Test]
+        public void Process_InvokeNextHandlerInChainSuccessfullyTest()
+        {
+            var firstHandler = new MessageReputationScoreHandler();
+            var nextHandler= new Mock<MessageReputationScoreHandler>();
+            firstHandler.SetNextHandler(nextHandler.Object);
+            var message = new EmailMessage
+            {
+                From = "johndoe@gmail.com",
+                Subject = "Test Subject",
+                To = "janedoe@gmail.com"
+            };
+            firstHandler.Process(message);
+            nextHandler.Verify(x => x.Process(message),Times.Once());
+        }
+    }
+}
---
+++ EmailMessageRouter.Domain.Test/Services/MessageRoutingServiceTest.cs
@@ -0,0 +1,84 @@
+﻿using EmailMessageRouter.Data.Cache;
+using EmailMessageRouter.Data.EntityModel;
+using EmailMessageRouter.Data.Repositories;
+using EmailMessageRouter.Domain.Model;
+using EmailMessageRouter.Domain.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace EmailMessageRouter.Domain.Test.Services
+{
+    public class MessageRoutingServiceTest
+    {
+        [Test]
+        public void ResolveEmailMessageType_NullOrEmptyEmailFromPropertySupplied()
+        {
+            var accountRepo = new Mock<IAccountRepository>();
+            var messageRequestRepo = new Mock<IMessageRequestRepository>();
+            var cache = new Mock<ICache<string, Account>>();
+            var service = new MessageRoutingServiceImpl(
+                accountRepo.Object,
+                messageRequestRepo.Object,
+                cache.Object
+            );
+            var message = new EmailMessage
+            {
+                From = "johndoe@gmail.com",
+                Subject = "Test Subject",
+                To = "janedoe@gmail.com",
+                HtmlBody = "some html content"
+            };
+            var account = new Account
+            {
+                AccountId = 1,
+                Email = message.From,
+                IsActive = true,
+                SupportedMessageType = 1
+            };
+            accountRepo
+                .Setup(x => x.FindByEmail(message.From))
+                .Returns(account);
+            cache
+                .Setup(x => x.Find(message.From))
+                .Returns(account);
+            message.From = null;
+            var messageType = service.ResolveEmailMessageType(message);
+            Assert.AreEqual(MessageType.Unknown, messageType);
+        }
+
+        [Test]
+        public void ResolveEmailMessageType()
+        {
+            var accountRepo = new Mock<IAccountRepository>();
+            var messageRequestRepo = new Mock<IMessageRequestRepository>();
+            var cache = new Mock<ICache<string, Account>>();
+            var service = new MessageRoutingServiceImpl(
+                accountRepo.Object,
+                messageRequestRepo.Object,
+                cache.Object
+            );
+            var message = new EmailMessage
+            {
+                From = "johndoe@gmail.com",
+                Subject = "Test Subject",
+                To = "janedoe@gmail.com",
+                HtmlBody = "some html content"
+            };
+            var account = new Account
+            {
+                AccountId = 1,
+                Email = message.From,
+                IsActive = true,
+                SupportedMessageType = 1
+            };
+            accountRepo
+                .Setup(x => x.FindByEmail(message.From))
+                .Returns(account);
+            cache
+                .Setup(x => x.Find(message.From))
+                .Returns(account);
+            var messageType = service.ResolveEmailMessageType(message);
+            Assert.AreEqual(MessageType.Transactional, messageType);
+        }
+    }
+}
