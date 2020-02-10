27450c5676c0ef250a833cbd8f46acb64727454f
Mario Dennis <mdennis876@gmail.com>
2020-02-10 08:41:48 -0500

    Update PostmarkEmail.cs

--- EmailMessageRouter.Web/EntityModel/PostmarkEmail.cs
+++ EmailMessageRouter.Web/EntityModel/PostmarkEmail.cs
@@ -5,16 +5,9 @@ namespace EmailMessageRouter.Web.EntityModel
 {
     public class PostmarkEmail
     {
-        [Required(ErrorMessage = "From property is required")]
         public string From { get; set; }
-        
-        [Required(ErrorMessage = "To property is required")]
         public string To { get; set; }
-        
-        [Required(ErrorMessage = "Subject property is required")]
         public string Subject { get; set; }
-        
-        [Required(ErrorMessage = "HtmlBody property is required")]
         public string HtmlBody { get; set; }
     }
 }
